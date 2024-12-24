62f97fceccbac1e4a04496256e88925c6e0e00fd
Luis Starlino <luis.guilherme009@gmail.com>
2024-12-24 07:32:37 -0300

    feat(main): inital configuration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,15 @@
-console.log("hello world");
+/*****************************************************************************************
+* @Author: Luis Starlino
+* @Date: 2024-12-24 07:20
+*****************************************************************************************/
+// ===== IMPORTS
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get("/", (req: Request, resp: Response)=> {
+
+    resp.send({message: "Hello World!"})
+})
+
+app.listen(3000)
